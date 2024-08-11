6b6990c80434f8277063856ca9ff423bc801aa1e
Otávio <omagalhaes032@gmail.com>
2024-08-10 22:37:07 -0300

    Add game-state.h file

---
+++ include/game-state.h
@@ -0,0 +1,13 @@
+#ifndef GAME_STATE_H
+
+#define GAME_STATE_H
+
+enum class GameState {
+    MainTitleScreen,
+    PlayingScreen,
+    PausedScreen,
+    WinningScreen,
+    GameOverScreen
+};
+
+#endif
