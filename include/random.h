83bc38232038be66f21419ebe32160e261c959a0
Otávio <omagalhaes032@gmail.com>
2024-08-10 22:35:03 -0300

    Struct Game in main.cpp

--- include/random.h
+++ include/random.h
@@ -2,14 +2,12 @@
 
 #define RANDOM_H
 
-#include <cmath>
 #include <random>
-#include <unordered_map>
 
 struct Random {
     static int gen(int min, int max) {
-        static mt19937 generator;
-        uniform_int_distribution<int> distribution(min, max);
+        static std::mt19937 generator;
+        std::uniform_int_distribution<int> distribution(min, max);
         int result = distribution(generator);
         return result;
     }
