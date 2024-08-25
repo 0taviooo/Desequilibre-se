fa45a13f476f410cd25d8b2ff0fface9cedf81e5
Otávio <omagalhaes032@gmail.com>
2024-08-24 22:16:47 -0300

    Add window parts values in constants

--- include/constants.h
+++ include/constants.h
@@ -2,11 +2,29 @@
 
 #define CONSTANTS_H
 
+#include <vector>
+#include <algorithm>
+
 struct GameConstants {
     static constexpr float blinkIterval = 1.f;
     static const int windowX = 708;
     static const int windowY = 640;
+    static const std::vector<float> windowXPieces;
+    static const std::vector<float> windowYPieces;
+    
+    static std::vector<float> generatePieces(int value) {
+        std::vector<float> v = {};
+        int num = 12;
+        for (int i = 1; i < num+1; ++i) {
+            v.push_back(windowX/num*i);
+        }
+        return v;
+    }
+
 };
+
+const std::vector<float> GameConstants::windowXPieces = GameConstants::generatePieces(GameConstants::windowX);
+const std::vector<float> GameConstants::windowYPieces = GameConstants::generatePieces(GameConstants::windowY);
 
 struct FontSize {
     static constexpr float h1 = 60.f;
--- main.cpp
+++ main.cpp
@@ -27,9 +27,9 @@ void Game::mainTitle(Color startColor) {
     const char* label_t1 = "(Des)Equilibrados!";
     const char* label_t2 = "Pressione Enter para começar!";
     const char* label_t3 = "IFCE Itapipoca";
-    DrawText(label_t1, Utils::centralizeText(label_t1, FontSize::h1, {0, GameConstants::windowX}), (GameConstants::windowY/12)*4, FontSize::h1, WHITE);
-    DrawText(label_t2, Utils::centralizeText(label_t2, FontSize::h2, {0, GameConstants::windowX}), (GameConstants::windowY/12)*7, FontSize::h2, startColor);
-    DrawText(label_t3, Utils::centralizeText(label_t3, FontSize::note, {0, GameConstants::windowX}), (GameConstants::windowY/12)*11, FontSize::note, WHITE);
+    DrawText(label_t1, Utils::centralizeText(label_t1, FontSize::h1, {0, GameConstants::windowX}), GameConstants::windowYPieces[3], FontSize::h1, WHITE);
+    DrawText(label_t2, Utils::centralizeText(label_t2, FontSize::h2, {0, GameConstants::windowX}), GameConstants::windowYPieces[6], FontSize::h2, startColor);
+    DrawText(label_t3, Utils::centralizeText(label_t3, FontSize::note, {0, GameConstants::windowX}), GameConstants::windowYPieces[9], FontSize::note, WHITE);
 }
 
 int main() {
